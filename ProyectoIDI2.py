e8638e677e45f02908dc06b456106e2855ddb193
Marcois <marcosfierros97@gmail.com>
2025-11-17 16:39:43 -0600

    Train test split with Pytorch

--- ProyectoIDI2.py
+++ ProyectoIDI2.py
@@ -1,2 +1,22 @@
-import numpy as np
-import pandas as pd
+from ImageDataset import ImageDataset 
+from torch.utils.data import DataLoader, random_split
+
+IMAGES_NUM=10
+
+dataset = ImageDataset(
+    csv_path="train.csv",
+    return_ela=True
+)
+
+train_size = int(0.7 * IMAGES_NUM)
+validation_size = int(0.15 * IMAGES_NUM)
+test_size = IMAGES_NUM - train_size - validation_size
+
+train_dataset, validation_dataset, test_dataset = random_split(dataset,[train_size, test_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+validation_loader = DataLoader(train_dataset, batch_size=32)
+test_loader = DataLoader(train_dataset, batch_size=32)
+
+for X, y in train_loader:
+    print(X)
